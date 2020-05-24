d6fbc602bff1ce48b267d92a79bdf1d4c250aea8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-24 10:14:36 -0700

    filter/validate email, int, script

--- filters.php
+++ filters.php
@@ -1,18 +1,69 @@
 <?php
+    // // check for post data
     // if (filter_has_var(INPUT_POST, 'data')) {
     //     echo 'Data Found';
     // } else {
     //     echo 'No Data';
     // }
 
-    if (filter_has_var(INPUT_POST, 'data')) {
-        if (filter_input(INPUT_POST, 'data', FILTER_VALIDATE_EMAIL)) {
-            echo 'Email valid';
-        } else {
-            echo 'Email is NOT valid';
-        }
-    }
+    // // check for valid email than using regex
+    // if (filter_has_var(INPUT_POST, 'data')) {
+    //     $email = $_POST['data'];
+
+    //     // remove illegal characters
+    //     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    //     echo $email.'<br>';
+
+    //     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //         echo 'Email valid';
+    //     } else {
+    //         echo 'Email is NOT valid';
+    //     }
+    // }
+
+    # FILTER_VALIDATE_BOOLEAN
+    # FILTER_VALIDATE_EMAIL
+    # FILTER_VALIDATE_FLOAT
+    # FILTER_VALIDATE_INT
+    # FILTER_VALIDATE_IP
+    # FILTER_VALIDATE_REGEXP
+    # FILTER_VALIDATE_URL
+
+    # FILTER_SANITIZE_EMAIL
+    # FILTER_SANITIZE_ENCODED
+    # FILTER_SANITIZE_NUMBER_FLOAT
+    # FILTER_SANITIZE_NUMBER_INT
+    # FILTER_SANITIZE_SPECIAL_CHARS
+    # FILTER_SANITIZE_STRING
+    # FILTER_SANITIZE_URL
+
+    // // integer validation
+    // # will take in both string or int
+    // $varInt = 100;
+    // $varStr = '100';
+
+    // if (filter_var($varInt, FILTER_VALIDATE_INT)) {
+    //     echo '$varInt: '.$varInt.' is a number';
+    // } else {
+    //     echo '$varInt: '.$varInt.' is NOT a number';
+    // }
+
+    // echo '<br>';
+
+    // if (filter_var($varStr, FILTER_VALIDATE_INT)) {
+    //     echo '$varStr: '.$varStr. ' is a number';
+    // } else {
+    //     echo '$varStr: '.$varStr. ' is NOT a number';
+    // }
+
+    $varScript = '<script>alert(1)</script>';
+    echo $varScript; // will execute alert script; <script>alert(1)</script>
+    echo filter_var($varScript, FILTER_SANITIZE_SPECIAL_CHARS); // will not execute and renders to harmless string; &#60;script&#62;alert(1)&#60;/script&#62;
+    // var_dump(filter_var($var, FILTER_SANITIZE_NUMBER_INT));
+
+    
 ?>
+
 <!-- $_SERVER['PHP_SELF'] will return current page -->
 <form method="POST" 
     action="
