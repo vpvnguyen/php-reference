8e72db9ae5b9877b51d53e87ef685db29cc82d7f
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-25 12:38:37 -0700

    add comments

--- website-sessions/page2.php
+++ website-sessions/page2.php
@@ -11,7 +11,7 @@
     }
 ?>
 
-
+<!-- html -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,6 +23,7 @@
     <div>Thank you <strong><?php echo $name; ?></strong>, you have subscribed with the email <strong><?php echo $email; ?></strong></div>
     <div>This page now has a session with your NAME and EMAIL. Refreshing the page will not clear.</div>
 
+    <!-- dashboard -->
     <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <input type="submit" name="submit" value="Go To Dashboard">
     </form>
--- website-sessions/page3.php
+++ website-sessions/page3.php
@@ -1,6 +1,11 @@
 <?php
     session_start();
     $name = $_SESSION['name'];
+
+    // on submit, redirect to page4.php
+    if (isset($_POST['submit'])) {
+        header('Location: page4.php');
+    }
 ?>
 
 <!-- html -->
@@ -13,5 +18,10 @@
 </head>
 <body>
     <h1>Heyo, <?php echo $name; ?></h1>
+
+    <!-- logout -->
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <input type="submit" name="submit" value="Log out">
+    </form>
 </body>
 </html>
