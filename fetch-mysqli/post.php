43216b96f225783a902cd0bec0af70a210440840
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-30 14:58:26 -0700

    used header, footer, navbar layout

--- fetch-mysqli/post.php
+++ fetch-mysqli/post.php
@@ -24,17 +24,7 @@
 ?>
 
 <!-- html -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Blog</title>
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-</head>
-<body>
-    
+<?php include('inc/header.php'); ?>
 
     <div class="container">
         <!-- href="/" will return back to root of htdocs. Need to configure to go back to whereever the root should be -->
@@ -56,5 +46,4 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-</body>
-</html>
+<?php include('inc/footer.php'); ?>
