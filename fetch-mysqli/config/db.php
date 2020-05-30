5e6e353ffcadc1e3698459303dd98ee9c2e6a059
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-30 14:56:56 -0700

    used config.php to apply creds

--- fetch-mysqli/config/db.php
+++ fetch-mysqli/config/db.php
@@ -1,9 +1,9 @@
 <?php
-    // get credentials; $dbPassword
-    require('creds.php');
+    // get credentials
+    // require('config.php'); // do not need this line because *.php already required('config.php');
 
     // create connection
-    $conn = mysqli_connect('localhost', 'root', $dbPassword, 'php_blog');
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
     // check connection
     if (mysqli_connect_errno()) {
