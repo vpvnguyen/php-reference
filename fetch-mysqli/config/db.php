9d2cc30f7d7bb4bce72d9ffc864d166780eb5e1b
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-30 14:07:22 -0700

    create connection to db

---
+++ fetch-mysqli/config/db.php
@@ -0,0 +1,13 @@
+<?php
+    // get credentials
+    require('creds.php');
+
+    // create connection
+    $conn = mysqli_connect('localhost', 'root', $dbPassword, 'php_blog');
+
+    // check connection
+    if (mysqli_connect_errno()) {
+        // connection failed
+        echo 'Failed to connect to MySQL '.mysqli_connect_errno();
+    }
+?>
