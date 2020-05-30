aac3362f7bc6f8609b258ac5fad8a14e3c36a9d5
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-30 14:56:36 -0700

    defined db creds

--- fetch-mysqli/config/config.php
+++ fetch-mysqli/config/config.php
@@ -1,4 +1,13 @@
 <?php
+    // get credentials; $dbPassword
+    require('creds.php');
+
     // define the root url
     define('ROOT_URL', 'http://localhost/php-reference/fetch-mysqli/');
+
+    // define db
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', $dbPassword);
+    define('DB_NAME', 'php_blog');
 ?>
