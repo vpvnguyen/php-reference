252a313982867913fddf539ba2f5b1b478d1dc10
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-31 10:41:34 -0700

    import config, header, navbar, footer

---
+++ crud-mysqli/index.php
@@ -0,0 +1,14 @@
+<?php
+    // import
+    require('config/config.php');
+    echo 'index.php';
+?>
+
+<!-- html -->
+<!-- header -->
+<?php include('inc/header.php'); ?>
+<!-- navbar -->
+<?php include('inc/navbar.php'); ?>
+<!-- body -->
+<!-- footer -->
+<?php include('inc/footer.php'); ?>
