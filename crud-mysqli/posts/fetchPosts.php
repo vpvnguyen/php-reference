7ccedfad1dd1770e0e945c1e59e4378ab4fec4e2
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-31 14:26:46 -0700

    add margin spacing between cards

--- crud-mysqli/posts/fetchPosts.php
+++ crud-mysqli/posts/fetchPosts.php
@@ -19,7 +19,7 @@
     <h1>Posts</h1>
     <?php foreach($posts as $post): ?>
         <div class="row">
-            <div class="card card-body bg-light">
+            <div class="card card-body bg-light m-2">
                 <h3><?php echo $post['title']; ?></h3>
                 <span>By <?php echo $post['author']; ?> on <?php echo $post['created_at']; ?></span>
                 <p class="m-3"><?php echo $post['body']; ?></p>
