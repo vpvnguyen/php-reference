34da961755ef9d90dd21cd01c4cd7ab1cf75eef8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-31 13:45:20 -0700

    add posts query

---
+++ crud-mysqli/posts/addPost.php
@@ -0,0 +1,63 @@
+<?php
+    require('../config/index.php');
+
+    // check for submit
+    if (isset($_POST['submit'])) {
+        echo 'submitted';
+        // get form data
+        $title = mysqli_real_escape_string($conn, $_POST['title']);
+        $body = mysqli_real_escape_string($conn, $_POST['body']);
+        $author = mysqli_real_escape_string($conn, $_POST['author']);
+
+        $query = "INSERT INTO posts(title, author, body) VALUES('$title', '$author', '$body')";
+
+        if (mysqli_query($conn, $query)) {
+            header('Location: '.ROOT_URL.'');
+        } else {
+            echo 'Error: '.mysqli_error($conn);
+        }
+    }
+
+    // // create query
+    // $query = 'SELECT * FROM posts';
+
+    // // get result
+    // $result = mysqli_query($conn, $query);
+
+    // // fetch data; data is in an associative array ['name' => 'Vincent'];
+    // $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    // // free result
+    // mysqli_free_result($result);
+
+    // // close connection
+    // mysqli_close($conn);
+?>
+
+<!-- html -->
+<!-- header & navbar -->
+<?php include('../inc/header.php'); ?>
+
+<div class="container mt-5">
+    <h1>Add Post</h1>
+    <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" name="title" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="author">Author</label>
+            <input type="text" name="author" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="body">Body</label>
+            <input type="text" name="body" class="form-control">
+        </div>
+        <input type="submit" name="submit" value="Submit" class="btn btn-primary">
+    </form>
+</div>
+
+<!-- footer -->
+<?php include('../inc/footer.php'); ?>
+
+
