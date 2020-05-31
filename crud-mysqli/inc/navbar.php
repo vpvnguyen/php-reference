ee790cc8bf1fac62583b7f299afd64f10e5a0d84
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-31 14:32:53 -0700

    renamed nav title; removed extra menu actions from psots

--- crud-mysqli/inc/navbar.php
+++ crud-mysqli/inc/navbar.php
@@ -1,7 +1,7 @@
 <?php echo '<br>navbar.php<br>'; ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">PHP Blog</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -13,13 +13,10 @@
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Menu
+          Posts
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="<?php echo ROOT_URL; ?>posts/addPost.php">Add Post</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
     </ul>
