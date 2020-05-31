93354576429ff3c37d53a42798290dd762e5075e
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-31 14:24:58 -0700

    fixed home route

--- crud-mysqli/inc/navbar.php
+++ crud-mysqli/inc/navbar.php
@@ -9,7 +9,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="<?php ROOT_URL; ?>">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="<?php echo ROOT_URL; ?>">Home</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
