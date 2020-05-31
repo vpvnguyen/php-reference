1882478a749833c74da08d37eaee8ffc4bd7672e
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-31 12:04:30 -0700

    moved navbar to header

--- crud-mysqli/index.php
+++ crud-mysqli/index.php
@@ -2,32 +2,15 @@
     // import
     require('config/index.php');
     echo '<br>index.php<br>';
-
-    // create query
-    $query = 'SELECT * FROM posts';
-
-    // get result
-    $result = mysqli_query($conn, $query);
-
-    // fetch data; data is in an associative array ['name' => 'Vincent'];
-    $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    
-    echo '<br>';
-    var_dump($posts);
-    echo '<br>';
-
-    // free result
-    mysqli_free_result($result);
-
-    // close connection
-    mysqli_close($conn);
 ?>
 
 <!-- html -->
-<!-- header -->
+<!-- header & navbar -->
 <?php include('inc/header.php'); ?>
-<!-- navbar -->
-<?php include('inc/navbar.php'); ?>
+
 <!-- body -->
+<div class="container mt-5">
+    <?php include('posts/fetchPosts.php'); ?>
+</div>
 <!-- footer -->
 <?php include('inc/footer.php'); ?>
